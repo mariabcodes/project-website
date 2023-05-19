d12cb70257e260bc5790d9d6934757a9e57fdb48
Maria Bielik <mariabielik@outlook.com>
2023-05-19 16:49:42 +0100

    Jordan Task 2

---
+++ active.js
@@ -0,0 +1,20 @@
+let topbutton = document.getElementById("topFunction");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if 
+    (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20
+    ) {
+     topbutton.style.display = "block";
+    } else {
+    topbutton.style.display = "none";
+    }
+}
+
+topbutton.addEventListener("click", backToTop);
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
